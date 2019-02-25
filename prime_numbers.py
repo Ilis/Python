92b1a92556885fe3eee9d168592a8ea6d0e84e04
Ilis <ilis@inbox.ru>
2019-02-25 10:39:42 +0500

    prime_numbers

---
+++ prime_numbers.py
@@ -0,0 +1,6 @@
+def prime_numbers(val):
+    return [
+        x for x in range(2, val+1) if all((x%y for y in range(2, 1+int(x**0.5)))            )
+        ]
+for n in prime_numbers(42):
+    print("%d" % n, end=" ")
