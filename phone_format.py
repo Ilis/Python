d6673ddc2912da9ad500a22c41dab8c4a23e7cf2
Ilis <ilis@inbox.ru>
2019-02-27 09:27:03 +0500

    phone_format

--- phone_format.py
+++ phone_format.py
@@ -1,9 +1,35 @@
 def phone_format(phone):
-    """Format tel number (fixed representation)
+    """Format tel number (fixed representation)"""
     
-    Formats only 10-digit number preceeding '+' and 1-digit number
-    """
-    return "%s%s %s%s%s %s%s%s-%s%s-%s%s" % tuple(list(phone))
+    l = len(phone)
 
-phone="+79998887766"
-print(phone_format(phone))
+    if l < 3:
+        return phone
+    elif l < 5:
+        return "%s-%s" % (phone[-4:-2], phone[-2:])
+    elif l < 8:
+        return "%s-%s-%s" % (phone[-7:-4], phone[-4:-2], phone[-2:])
+    elif l < 10:
+        return "%s %s-%s-%s" % (phone[-9:-7], phone[-7:-4], phone[-4:-2], phone[-2:])
+    elif l == 10:
+        if phone[0] == '9':
+            return "%s %s-%s-%s" % (phone[-10:-7], phone[-7:-4], phone[-4:-2], phone[-2:])
+        else:
+            return "(%s)%s-%s-%s" % (phone[-10:-7], phone[-7:-4], phone[-4:-2], phone[-2:])
+    else:
+        return "%s %s %s-%s-%s" % (phone[:-10], phone[-10:-7], phone[-7:-4], phone[-4:-2], phone[-2:])
+
+for s in ["+839987654321",
+          "+73437654321",
+          "9987654321",
+          "3437654321",
+          "87654321",
+          "7654321",
+          "654321",
+          "54321",
+          "4321",
+          "321",
+          "21",
+          "1",
+          ]:
+    print(phone_format(s))
