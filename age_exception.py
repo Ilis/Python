b227ff66353e59271fbc1036a1f03b352cea7f23
Ilis <ilis@inbox.ru>
2019-02-27 12:01:46 +0500

    a age_exception

---
+++ age_exception.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        s = input("Сколько вам лет?: ")
+        age = int(s)
+        if age < 0:
+            print(f"Ха-ха, {age} — это меньше нуля! Серьёзно!")
+        elif age > 199:
+            print(f"Да ладно, {age} — это слишком много! Серьёзно!")
+        else:
+            break
+    except ValueError:
+        print(f"«{s}» лет? Неееет... Ещё раз:")
+print(f"Ваш возраст — {age}!")
